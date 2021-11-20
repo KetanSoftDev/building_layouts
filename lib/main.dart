b59321ddff54fad5fba89315b00f01b49f85a861
Ketan Shukla <ketansoftdev@gmail.com>
2021-11-20 00:24:48 -0800

    changed app bar title and app title

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Layout Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Layout Demo'),
     );
   }
 }
